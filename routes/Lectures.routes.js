9a3da396b3fe1a08a25a18b96137843c1923d43f
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-29 23:29:21 +0000

    ✨ Add PUT and DELETE routes for lectures in Lectures.routes.js

--- routes/Lectures.routes.js
+++ routes/Lectures.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
 //const pool = require('../db');
-import { getLectures, postLectures, getLecturesById } from '../controllers/Lectures.controllers.js';
+import { getLectures, postLectures, getLecturesById, putLectures} from '../controllers/Lectures.controllers.js';
 
 router.get('/lectures', getLectures);
 router.get('/lectures/:id', getLecturesById);
 router.post('/lectures', postLectures);
+router.put('/lectures/:id', putLectures);
+router.delete('/lectures/:id', deleteLectures);
 
 export default router;
